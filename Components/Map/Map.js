a70ee6f3e0f9e91ced283ed8dcaa1e0f8fca6d8e
Kane <Phoboes@live.com>
2021-07-19 15:09:26 +1000

    init

--- Components/Map/Map.js
+++ Components/Map/Map.js
@@ -1,17 +1,37 @@
 // import L from "leaflet";
-import { MapContainer, MapControl } from "react-leaflet";
+import { MapContainer, MapControl, FeatureGroup } from "react-leaflet";
 import styles from "./Map.module.css";
 import TileLayers from "./TileLayers/TileLayers";
+import { EditControl } from "react-leaflet-draw";
 
 const Map = (props) => {
+  // const reactFGref = useRef();
+
   return (
     <div>
       <MapContainer
         center={[-34.040441, 151.1988752]}
         zoom={13}
-        className={styles["leaflet-container"]}
-      >
+        className={styles["leaflet-container"]}>
         <TileLayers />
+        <FeatureGroup>
+          <EditControl
+            position="topright"
+            // onEdited={this._onEdited}
+            // onCreated={this._onCreated}
+            // onDeleted={this._onDeleted}
+            // onMounted={this._onMounted}
+            // onEditStart={this._onEditStart}
+            // onEditStop={this._onEditStop}
+            // onDeleteStart={this._onDeleteStart}
+            // onDeleteStop={this._onDeleteStop}
+            draw={
+              {
+                // rectangle: false,
+              }
+            }
+          />
+        </FeatureGroup>
       </MapContainer>
     </div>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import "leaflet/dist/leaflet.css";
-// import "leaflet-draw/dist/leaflet.draw.css";
+import "leaflet-draw/dist/leaflet.draw.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
